0d086bf672665a9e3aedd50b3a7f449fbb6c9aa9
Taejung Kim <hi2177@naver.com>
2022-03-07 17:57:43 +0900

    ✨ feature: LikeVideoTab 구현

--- src/components/domain/Tabs/Like/LikeVideoTab.tsx
+++ src/components/domain/Tabs/Like/LikeVideoTab.tsx
@@ -1,40 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { setItem, getItem, addItem } from '@/hooks/storage';
-import VideoList from '../../../base/VideoList';
+import React, { useState } from 'react';
+import { setItem, getItem } from '@/hooks/storage';
 import { IVideo } from '@/types/IVideo';
-import { Grid, IconButton } from '@mui/material';
+import { IconButton } from '@mui/material';
 import VideoGrid from '@/components/base/VideoGrid';
 import {
-  FavoriteBorder as FavoriteBorderIcon,
   Favorite as FavoriteIcon,
 } from "@mui/icons-material";
-
-
+import { storageHandleLike } from '@/hooks/StorageHandle';
 
 const LikeVideoTab = () => {
-  const [isLike, setIsLike] = useState(false);
-  const [likeVideos, setLikeVideos] = useState(getItem("videos"));
-  useEffect(() => {
-    setLikeVideos(getItem("videos").filter((video: IVideo) => video.status.like !== true));
-    return
-  }, [])
+  const [likeVideos, setLikeVideos] = useState(getItem("videos").filter((video: IVideo) => video.status.like === true));
 
   const handleLike = (e: React.MouseEvent) => {
-    console.log(e)
+    const id = e.currentTarget.parentElement?.parentElement?.dataset.id;
+    setItem("videos", JSON.stringify(getItem('videos').map((video: IVideo) => video.videoId === id ? storageHandleLike(video) : video)));
+    setLikeVideos(getItem("videos").filter((video: IVideo) => video.status.like === true));
   }
 
-  
   return (
     <VideoGrid videos={likeVideos}>
-    {isLike ?
-        <IconButton color="secondary" size="small" >
-          <FavoriteIcon fontSize="small" />
-        </IconButton>
-        :
-        <IconButton color="secondary" size="small" >
-          <FavoriteBorderIcon fontSize="small" />
-        </IconButton>
-      }
+      <IconButton color="secondary" size="small" >
+        <FavoriteIcon fontSize="small" onClick={handleLike}/>
+      </IconButton>
     </VideoGrid>
   );
 }
